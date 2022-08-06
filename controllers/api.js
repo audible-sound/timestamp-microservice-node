e2595635dcd1a2dc43af286897537fccb4279128
Audy Wallace Siegle <audYble@gmail.com>
2022-08-06 20:08:33 +0700

    format prettier

--- controllers/api.js
+++ controllers/api.js
@@ -1,25 +1,24 @@
 class ApiController {
-    static getDate(req, res) {
-        try {
-            const dateParam =  req.params.date;
-            let date = null;
-            if (isNaN(+dateParam) === true) {
-                date = new Date(dateParam);
-            } else {
-                date = new Date(+dateParam);
-            }
-            if (isNaN(date.getTime())) throw new Error("Invalid Date")
-            res.status(200).send({
-                "unix": date.getTime(),
-                "utc": date.toUTCString()
-            })
-            
-        } catch (error) {
-            res.status(500).send({
-                error: error.message
-            })
-        }
+  static getDate(req, res) {
+    try {
+      const dateParam = req.params.date;
+      let date = null;
+      if (isNaN(+dateParam) === true) {
+        date = new Date(dateParam);
+      } else {
+        date = new Date(+dateParam);
+      }
+      if (isNaN(date.getTime())) throw new Error("Invalid Date");
+      res.status(200).send({
+        unix: date.getTime(),
+        utc: date.toUTCString(),
+      });
+    } catch (error) {
+      res.status(500).send({
+        error: error.message,
+      });
     }
+  }
 }
 
 module.exports = ApiController;
